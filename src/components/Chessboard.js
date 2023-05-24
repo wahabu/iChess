7aa780fa6d459b6ad2178f5c230820bf9a74b085
wahabu <ritscg2hibo@gmail.com>
2023-05-24 10:50:59 +0900

    Update div tag with chessboard id

--- src/components/Chessboard.js
+++ src/components/Chessboard.js
@@ -6,5 +6,5 @@ const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8];
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 export default function Chessboard() {
-  return <div>Hello i am the chessboard!!!!!!</div>
+  return <div id="chessboard"></div>
 }
