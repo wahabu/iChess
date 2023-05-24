0c3a61990d2724dca0d4bc958ca8084d4726901e
wahabu <ritscg2hibo@gmail.com>
2023-05-24 15:52:56 +0900

    Updated the Tile.css and Tile.js files, and adjusted the image dimensions

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -6,7 +6,7 @@ import './Tile.css';
 
 export default function Tile({number}) {
   if (number % 2 === 0) {
-    return <div className="tile black-tile"><img src="./assets/images/bp.png" /></div>;
+    return <div className="tile black-tile"><img src="assets/images/bp.png" /></div>;
   } else {
     return <div className="tile white-tile"></div>;
   }
