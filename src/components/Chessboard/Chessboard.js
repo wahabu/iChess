6f63aa5c81cf7278f720dc393c552bd44f6f42a1
wahabu <ritscg2hibo@gmail.com>
2023-05-24 15:40:23 +0900

    Moved the code contents to the appropriate location after creating the new folders

--- src/components/Chessboard/Chessboard.js
+++ src/components/Chessboard/Chessboard.js
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import Tile from "../Tile/Tile";
 import './Chessboard.css';
 
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
@@ -12,20 +11,7 @@ export default function Chessboard() {
     for (let i = 0; i < horizontalAxis.length; i++) {
       const number = j + i + 2;
       
-      if(number % 2 === 0) {
-          board.push(
-      //       <div className="tile black-tile">[{horizontalAxis[i]} {verticalAxis[j]}]</div>
-      //     )
-      // } else {
-      //     board.push(
-      //       <div className="tile white-tile">[{horizontalAxis[i]} {verticalAxis[j]}]</div>
-                <div className="tile black-tile"></div>
-            )
-          } else {
-              board.push(
-                <div className="tile white-tile"></div>
-            );
-          }
+      board.push(<Tile number={number} />);
     }
   }
 
