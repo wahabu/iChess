7e592edba42f59dc21011021b280b019686b5826
wahabu <ritscg2hibo@gmail.com>
2023-05-25 09:50:41 +0900

    Refactored Tile component to accept image as a prop for more flexibility

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -4,10 +4,18 @@ import './Tile.css';
 //   number: number;
 // }
 
-export default function Tile({number}) {
+export default function Tile({number, image}) {
   if (number % 2 === 0) {
-    return <div className="tile black-tile"><img src="assets/images/bp.png" /></div>;
+    return (
+      <div className="tile black-tile">
+        <img src={image} />
+      </div>
+    );
   } else {
-    return <div className="tile white-tile"></div>;
+    return (
+      <div className="tile white-tile">
+        <img src={image} />
+      </div>
+    );
   }
 }
