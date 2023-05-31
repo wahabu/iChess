a4ad158a1c44edf6a32eabff64cdda3d20c8aa5c
wahabu <ritscg2hibo@gmail.com>
2023-05-31 23:17:21 +0900

    Added all pieces to the initial game setup in the Chessboard component

--- src/components/Chessboard/Chessboard.js
+++ src/components/Chessboard/Chessboard.js
@@ -26,6 +26,19 @@ pieces.push({image: "assets/images/br.png", x: 0, y: 7})
 pieces.push({image: "assets/images/br.png", x: 7, y: 7})
 pieces.push({image: "assets/images/bn.png", x: 6, y: 7})
 pieces.push({image: "assets/images/bn.png", x: 1, y: 7})
+pieces.push({image: "assets/images/bb.png", x: 2, y: 7})
+pieces.push({image: "assets/images/bb.png", x: 5, y: 7})
+pieces.push({image: "assets/images/bq.png", x: 4, y: 7})
+pieces.push({image: "assets/images/bk.png", x: 3, y: 7})
+
+pieces.push({image: "assets/images/wr.png", x: 0, y: 0})
+pieces.push({image: "assets/images/wr.png", x: 7, y: 0})
+pieces.push({image: "assets/images/wn.png", x: 6, y: 0})
+pieces.push({image: "assets/images/wn.png", x: 1, y: 0})
+pieces.push({image: "assets/images/wb.png", x: 2, y: 0})
+pieces.push({image: "assets/images/wb.png", x: 5, y: 0})
+pieces.push({image: "assets/images/wq.png", x: 4, y: 0})
+pieces.push({image: "assets/images/wk.png", x: 3, y: 0})
 
 function Chessboard() {
   let board = [];
